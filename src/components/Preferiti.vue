b46f04329df62624be342dd384b883662f2a9ad5
lucastoian <lu.stoian@reply.it>
2023-08-09 12:43:45 +0200

    go

--- src/components/Preferiti.vue
+++ src/components/Preferiti.vue
@@ -1,5 +1,5 @@
 <template>
-<p>preferiti</p>
+
     <div class="container" style="display: flex; flex-wrap: wrap;">
         <ProductCard v-for="product in this.preferiti" v-model:name="product.name"  v-model:price="product.price" v-model:discount="product.discount" v-model:mpn="product.mpn" v-model:color="product.color" v-model:brandname="product.brandname" v-model:image="product.image" ></ProductCard>
      
